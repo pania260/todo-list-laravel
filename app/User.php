c415fa8e68c87fb6f959de2ee41f0c6d44bc1c7b
parnian1 <parnian1381@gmail.com>
2018-09-18 15:02:34 +0430

    it has problem in login

--- app/User.php
+++ app/User.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace App;
+
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+
 class User extends Authenticatable
 {
     use Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -13,6 +17,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -21,6 +26,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
     public function todos()
     {
         return $this->hasMany('App\Todo', 'user_id');
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,6 +55,9 @@
                                         Login
                                     </button>
 
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        Forgot Your Password?
+                                    </a>
                                 </div>
                             </div>
                         </form>
